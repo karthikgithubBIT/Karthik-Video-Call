e9623aabaf47c082d31444081648653d7e008a02
karthikgithubBIT <karthikdabhi78@gmail.com>
2025-12-09 12:30:14 +0530

    Fix: replace submodule with actual frontend folder

---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,93 @@
+import axios from "axios";
+import httpStatus from "http-status";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import server from "../environment";
+
+export const AuthContext = createContext({});
+
+const client = axios.create({
+  baseURL: `${server}/api/v1/users`,
+});
+
+export const AuthProvider = ({ children }) => {
+  const authContext = useContext(AuthContext);
+  const [userData, setUserData] = useState(authContext);
+  const router = useNavigate();
+
+  const handleRegister = async (name, username, password) => {
+    try {
+      let request = await client.post("/register", {
+        name,
+        username,
+        password,
+      });
+
+      if (request.status === httpStatus.CREATED) {
+        return request.data.message;
+      }
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  const handleLogin = async (username, password) => {
+    try {
+      let request = await client.post("/login", {
+        username,
+        password,
+      });
+
+      if (request.status === httpStatus.OK) {
+        localStorage.setItem("token", request.data.token);
+        router("/home");
+      }
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  const getHistoryOfUser = async () => {
+    try {
+      let request = await client.get("/get_all_activity", {
+        params: {
+          token: localStorage.getItem("token"),
+        },
+      });
+
+      const response = request.data;
+
+      // ALWAYS return array
+      if (Array.isArray(response)) return response;
+      if (Array.isArray(response.data)) return response.data;
+      if (Array.isArray(response.history)) return response.history;
+
+      return [];
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  const addToUserHistory = async (meetingCode) => {
+    try {
+      let request = await client.post("/add_to_activity", {
+        token: localStorage.getItem("token"),
+        meeting_code: meetingCode,
+      });
+      return request;
+    } catch (e) {
+      throw e;
+    }
+  };
+
+  const data = {
+    userData,
+    setUserData,
+    addToUserHistory,
+    getHistoryOfUser,
+    handleRegister,
+    handleLogin,
+  };
+
+  return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
+};
